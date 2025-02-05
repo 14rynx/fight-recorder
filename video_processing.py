194294c627a420434b4a67e66f5385d85f6c99a1
Larynx Austrene <larynx.austrene@gmail.com>
2025-02-05 23:41:50 +0100

    Rename after concatenation to not run into file system delays.

--- video_processing.py
+++ video_processing.py
@@ -18,6 +18,7 @@ class ProcessingElement:
         self.recording_path = recording_path
         self.output_dir = output_dir
         self.output_name = output_name
+        self.renamed = False
 
     @property
     def extension(self):
@@ -26,15 +27,15 @@ class ProcessingElement:
 
     @property
     def replay_destination(self):
-        return os.path.join(self.output_dir, f"{self.output_name}_replay{self.extension}").replace("\\","/")
+        return os.path.join(self.output_dir, f"{self.output_name}_replay{self.extension}").replace("\\", "/")
 
     @property
     def recording_destination(self):
-        return os.path.join(self.output_dir, f"{self.output_name}_recording{self.extension}").replace("\\","/")
+        return os.path.join(self.output_dir, f"{self.output_name}_recording{self.extension}").replace("\\", "/")
 
     @property
     def concatenated_destination(self):
-        return os.path.join(self.output_dir, f"{self.output_name}_concatenated{self.extension}").replace("\\","/")
+        return os.path.join(self.output_dir, f"{self.output_name}_concatenated{self.extension}").replace("\\", "/")
 
 
 class VideoProcessingPipeline:
@@ -50,7 +51,14 @@ class VideoProcessingPipeline:
 
     def process(self, replay_path, recording_path, output_dir, output_name):
         video_element = ProcessingElement(replay_path, recording_path, output_dir, output_name)
-        self.rename_in_thread(video_element)
+
+        if self.auto_concatenate:
+            self.concatenate_in_thread(video_element)
+            if not self.delete:
+                self.rename_in_thread(video_element)
+        else:
+            self.rename_in_thread(video_element)
+            self.concatenate_candidate_elements.append(video_element)
 
     def rename(self, video_element):
         if os.path.exists(video_element.replay_path):
@@ -65,67 +73,69 @@ class VideoProcessingPipeline:
                 except PermissionError:
                     time.sleep(10)
 
-        # Continue with concatenate if needed
-        if self.auto_concatenate:
-            self.concatenate_in_thread(video_element)
-        else:
-            self.concatenate_candidate_elements.append(video_element)
-
     def rename_in_thread(self, video_element):
         rename_thread = threading.Thread(target=self.rename, args=(video_element,))
         rename_thread.start()
 
     def concatenate(self, video_element):
-        if os.path.exists(video_element.replay_destination) and os.path.exists(video_element.recording_destination):
-            self.status_callback(ProcessingStatusCallback.PROCESSING_STARTED)
+        self.status_callback(ProcessingStatusCallback.PROCESSING_STARTED)
 
-            concat_directory = os.path.join(self.files_path, "concat.txt")
+        concat_directory = os.path.join(self.files_path, "concat.txt")
 
-            try:
-                with open(concat_directory, 'w') as concat_file:
-                    concat_file.writelines([
-                        f"file '{video_element.replay_destination}'\n",
-                        f"file '{video_element.recording_destination}'"
-                    ])
+        if video_element.renamed:
+            replay_input = video_element.replay_destination
+            recording_input = video_element.recording_destination
+        else:
+            replay_input = video_element.replay_path
+            recording_input = video_element.recording_path
 
-                command = [
-                    "ffmpeg",
-                    "-f", "concat",
-                    "-safe", "0",
-                    "-i", concat_directory,
-                    "-c", "copy",
-                    video_element.concatenated_destination
-                ]
-
-                creation_flags = 0
-                if sys.platform == "win32":
-                    creation_flags = subprocess.CREATE_NO_WINDOW
-
-                process = subprocess.Popen(
-                    command,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    text=True,
-                    creationflags=creation_flags
-                )
-
-                _, stderr = process.communicate()
-                if process.returncode != 0:
-                    self.status_callback((ProcessingStatusCallback.PROCESSING_ERROR, stderr))
+        try:
+            with open(concat_directory, 'w') as concat_file:
+                if not os.path.exists(replay_input) or \
+                        not os.path.exists(recording_input):
+                    self.logger.error("Could not start processing because input files did not exist!")
                     return
 
-                os.remove(concat_directory)
+                concat_file.writelines([
+                    f"file '{replay_input}'\n",
+                    f"file '{recording_input}'"
+                ])
 
-                if self.delete:
-                    os.remove(video_element.replay_destination)
-                    os.remove(video_element.recording_destination)
+            command = [
+                "ffmpeg",
+                "-f", "concat",
+                "-safe", "0",
+                "-i", concat_directory,
+                "-c", "copy",
+                video_element.concatenated_destination
+            ]
 
-                self.status_callback(ProcessingStatusCallback.PROCESSING_ENDED)
-            except Exception as e:
-                self.status_callback((ProcessingStatusCallback.PROCESSING_ERROR, e))
-        else:
-            self.logger.warning("Could not start processing because files did not exist")
+            creation_flags = 0
+            if sys.platform == "win32":
+                creation_flags = subprocess.CREATE_NO_WINDOW
 
+            process = subprocess.Popen(
+                command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                creationflags=creation_flags
+            )
+
+            _, stderr = process.communicate()
+            if process.returncode != 0:
+                self.status_callback((ProcessingStatusCallback.PROCESSING_ERROR, stderr))
+                return
+
+            os.remove(concat_directory)
+
+            if self.delete:
+                os.remove(replay_input)
+                os.remove(recording_input)
+
+            self.status_callback(ProcessingStatusCallback.PROCESSING_ENDED)
+        except Exception as e:
+            self.status_callback((ProcessingStatusCallback.PROCESSING_ERROR, e))
 
     def concatenate_in_thread(self, video_element):
         video_processing_thread = threading.Thread(target=self.concatenate, args=(video_element,))
